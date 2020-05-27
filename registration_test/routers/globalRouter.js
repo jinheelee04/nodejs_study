7f4c79d6a871b7aba2f1e08e87800b8f78775f3c
jinheelee04 <2pro.jlee@gmail.com>
2020-05-27 14:51:41 +0900

    0527 회원정보 및 공간정보 수정삭제 기능 추가

--- registration_test/routers/globalRouter.js
+++ registration_test/routers/globalRouter.js
@@ -33,18 +33,150 @@ globalRouter.get("/sign_up", function(req, res, next){
 globalRouter.post("/sign_up", function(req, res,next){
     var body = req.body;
     console.log("body=",req.body);
-    // let result = models.user.create({
-    //     clientID : body.clientID,
-    //     clientName : body.clientName
-    // })
+  
+    connection.query("INSERT INTO client_info (client_id, client_name) VALUES (?, ?)", 
+    [body.clientID, body.clientName], function(err, result, field){
+        if(err){
+            console.log(err);
+            res.status(500).send('Internal Server  Error');
+        }else{
+            console.log('client 정보 등록 성공');
+            res.redirect("/");
 
-    // res.redirect("/user/sign_up");
-    connection.query("INSERT INTO client_info (client_id, client_name) VALUES (?, ?)", [
-        body.clientID, body.clientName
-      ], function(){
-        res.redirect("/");
+        }
       });
 })
+
+//client 정보 수정 post
+globalRouter.post("/clients/update", function(req, res,next){
+    var id = req.body.id;
+    var name = req.body.name;
+    console.log("body=",req.body);
+    var sql = "update client_info set client_name=? where client_id = ?"
+    connection.query(sql, 
+    [name, id], function(err, result, field){
+        if(err){
+            console.log(err);
+            res.status(500).send('Internal Server  Error');
+        }else{
+            console.log('client 정보 수정 성공');
+            res.send({result:true, id:id, name:name});
+        }
+      });
+})
+//client 정보 삭제 post
+globalRouter.post("/clients/delete", function(req, res,next){
+    var data = req.body.data;
+    console.log("data=", data);
+
+    var sql = 'delete from client_info where client_id = ?';
+    connection.query(sql, data, function(err, result, field){
+        if(err){
+            console.log(err);
+            res.status(500).send('Internal Server  Error');
+        }else{
+            console.log('client 정보 삭제 성공');
+            res.send({result:true, data:data});
+
+        }
+      });
+
+
+})
+
+
+//zone 정보 등록 get
+globalRouter.get("/zone/add", function(req, res, next){
+    res.render('zone_add', {title:'zone add page'});
+});
+
+
+//zone 정보 등록 post
+globalRouter.post("/zone/add", function(req, res,next){
+    var body = req.body;
+    console.log("body=",req.body);
+    var sql = "INSERT INTO zone_info (client_id, zone_id, zone_name) VALUES (?,?,?)";
+
+    connection.query(sql, 
+        [body.clientID, body.zoneID, body.zoneName], function(err, result, field){
+        if(err){
+            console.log(err);
+            res.status(500).send('Internal Server  Error');
+        }else{
+            console.log('zone 정보 등록 성공');
+            res.redirect("/");
+        }
+      });
+})
+
+//zone 정보 수정 post
+globalRouter.post("/zone/update", function(req, res,next){
+    var id = req.body.id;
+    var zoneID = req.body.zoneID;
+    var name = req.body.name;
+    console.log("body=",req.body);
+    var sql = "update zone_info set zone_id=?, zone_name=? where id = ?"
+    connection.query(sql, 
+    [zoneID, name, id], function(err, result, field){
+        if(err){
+            console.log(err);
+            res.status(500).send('Internal Server  Error');
+        }else{
+            console.log('zone 정보 수정 성공');
+            res.send({result:true, id:id, name:name, zoneID:zoneID});
+        }
+      });
+})
+//zone 정보 삭제 post
+globalRouter.post("/zone/delete", function(req, res,next){
+    var id = req.body.id;
+    console.log("body=", req.body);
+
+    var sql = 'delete from zone_info where id = ?';
+    connection.query(sql, id, function(err, result, field){
+        if(err){
+            console.log(err);
+            res.status(500).send('Internal Server  Error');
+        }else{
+            console.log('zone 정보 삭제 성공');
+            res.send({result:true, id:id});
+
+        }
+      });
+
+
+})
+
+//전체회원 조회 get
+globalRouter.get("/clients", function(req, res, next){
+    connection.query("SELECT * FROM client_info;", function(err, result, fields){
+        if(err){
+          console.log("쿼리문에 오류가 있습니다.");
+        }
+        else{
+          console.log('The solution is: ', result);
+          //render() 메서드를 호출 할 때, 쿼리 결과인 products 테이블의 모든 데이터를 콜백 함수에 result 인자로 넘겨준다.
+          //result에는 products 테이블의 데이터들이 배열의 형태로 담겨있다.
+          res.render('clients', {title:'clients info page', results : result});
+        }
+      });
+});
+
+//아이공간 조회 get
+globalRouter.get("/zone", function(req, res, next){
+    var sql ="select z.id, c.client_id, c.client_name, z.zone_id, z.zone_name from client_info AS c join zone_info as z on c.client_id = z.client_id";
+    connection.query(sql, function(err, result, fields){
+        if(err){
+          console.log("쿼리문에 오류가 있습니다.");
+        }
+        else{
+            console.log('The solution is: ', result);
+          //render() 메서드를 호출 할 때, 쿼리 결과인 products 테이블의 모든 데이터를 콜백 함수에 result 인자로 넘겨준다.
+          //result에는 products 테이블의 데이터들이 배열의 형태로 담겨있다.
+          res.render('zone', {title:'zone info page', results : result});
+        }
+      });
+});
 
 
 
@@ -76,19 +208,27 @@ globalRouter.post("/login", async function(req, res, next){
     }
 });
 
-//전체회원 조회 get
-globalRouter.get("/clients", function(req, res, next){
-    connection.query("SELECT * FROM client_info;", function(err, result, fields){
-        if(err){
-          console.log("쿼리문에 오류가 있습니다.");
-        }
-        else{
-          //render() 메서드를 호출 할 때, 쿼리 결과인 products 테이블의 모든 데이터를 콜백 함수에 result 인자로 넘겨준다.
-          //result에는 products 테이블의 데이터들이 배열의 형태로 담겨있다.
-          res.render('clients', {title:'clients info page', results : result});
-        }
-      });
+//ajax 테스트
+globalRouter.get('/ajax_test', function(req, res, next) {
+    res.render("ajax_test");
 });
+
+
+//ajax 테스트
+/* POST 호출 처리 */
+globalRouter.post('/ajax', function(req, res, next) {
+
+    console.log('POST 방식으로 서버 호출됨');
+    //view에 있는 data 에서 던진 값을 받아서
+    var msg = req.body.msg;
+    msg = '[에코]' + msg;
+
+    //json 형식으로 보내 준다.
+
+    res.send({result:true, msg:msg});
+
+});
+
 
 
 module.exports = globalRouter;
--- was_test/index.js
+++ was_test/index.js
@@ -26,27 +26,43 @@ app.get('/clients', function(req, res){
     console.log('The solution is: ', rows);
     res.send(rows);
   });
-  connection.end();
+
+});
+//client 삭제
+app.get('/clients/delete', function(req, res){
+  var sql = 'delete from client_info where client_id = ?';
+  var value = 1;
+  connection.query(sql, value, function(err, result, field){
+    if(err){
+      console.log(err);
+      res.status(500).send('Internal Server  Error');
+    }
+    console.log("delete 성공");
+    res.redirect('/success/del');
+  });
+
 });
 
 //client 및 zone 정보조회
 app.get('/zone', function(req, res){
-  var sql ="select c.client_id, c.client_name, z.zone_id, z.zone_name from client_info AS c join zone_info as z on c.client_id = z.client_id";
+  var sql ="select z.id, c.client_id, c.client_name, z.zone_id, z.zone_name from client_info AS c join zone_info as z on c.client_id = z.client_id";
   connection.query(sql, function(err, rows) {
     if(err) throw err;
 
     console.log('The solution is: ', rows);
     res.send(rows);
   });
-  connection.end();
+
 });
 
 //client_info 테이블  데이터 추가
 app.get('/clients/add',(req, res)=>{
   var sql = "INSERT INTO client_info (client_id, client_name) VALUES ?";
   var values = [
-    [ 10, 'test1'],
-    [ 11, 'test2']
+    [ 1, '송경민'],
+    [ 2, '김지훈'],
+    [ 3, '김용수'],
+    [ 4, '이진희']
   ];
 
   connection.query(sql, [values], function(err, result, field){
@@ -54,17 +70,19 @@ app.get('/clients/add',(req, res)=>{
       console.log(err);
       res.status(500).send('Internal Server  Error');
     }
+    console.log("insert 성공");
     res.redirect('/success');
   });
-  connection.end();
+ 
 });
 
 //zone_info 테이블  데이터 추가
 app.get('/zone/add',(req, res)=>{
   var sql = "INSERT INTO zone_info (client_id, zone_id, zone_name) VALUES ?";
   var values = [
-    [ 1, 1, 'zone1'],
-    [ 1, 2,'zone2']
+    [ 2, 4001, '피아노학원'],
+    [ 3, 4002,'수학학원'],
+    [ 1, 4003, '남강빌딩']
   ];
 
   connection.query(sql, [values], function(err, result, field){
@@ -72,14 +90,19 @@ app.get('/zone/add',(req, res)=>{
       console.log(err);
       res.status(500).send('Internal Server  Error');
     }
+    console.log("insert 성공");
     res.redirect('/success');
   });
-  connection.end();
+
 });
 
-//성공시 메세지 출력
+//insert 성공시 메세지 출력
 app.get('/success', function(req, res){
   res.send('insert 성공');
+});
+//delete 성공시 메세지 출력
+app.get('/success/del', function(req, res){
+  res.send('delete 성공');
 });
 
 app.listen(app.get('port'), function () {
